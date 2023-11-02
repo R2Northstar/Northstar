9b772065918dd557245890d24f08fb41293bae59
GeckoEidechse <gecko.eidechse+git@pm.me>
2023-11-02 16:35:53 +0100

    feat: Add debug prints to wait script

--- wait_for_launcher_dl.sh
+++ wait_for_launcher_dl.sh
@@ -9,9 +9,12 @@ fi
 url="https://github.com/R2Northstar/NorthstarLauncher/releases/download/$1/northstar-launcher.zip"
 wait_time_seconds=60
 
+echo "Checking $url"
+
 # Loop until the response code changes
 while true; do
     response=$(curl --silent --output /dev/null --write-out "%{http_code}" $url)
+    echo "Got response $response"
     if [ $response -eq 404 ]; then
         echo "Response is 404. Retrying in $wait_time_seconds seconds."
         sleep $wait_time_seconds
